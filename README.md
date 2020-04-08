04c6cd64c1128da556a93f0091355a9fcdaf9298
qing.liu <qing.liu@its.cn>
2020-04-08 23:56:00 +0800

    定制andt主题

--- README.md
+++ README.md
@@ -32,3 +32,5 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 全局引入的组件
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './style/style.less';
 
 Vue.config.productionTip = false;
 
