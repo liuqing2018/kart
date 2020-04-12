5696a96617d8eb683d6035005917605056ccd6e6
qing.liu <qing.liu@its.cn>
2020-04-12 18:29:04 +0800

    init

---
+++ src/components/frame/QueryFrame.vue
@@ -0,0 +1,38 @@
+/**
+* Created by tudou on 2020/4/12 12:27.
+*/
+<template>
+  <div class="query-container">
+    <div class="query-condition">
+
+    </div>
+
+    <div class="query-result">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QueryFrame',
+  components: {},
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {
+  },
+  mounted() {
+  },
+  destroyed() {
+  },
+  methods: {}
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
