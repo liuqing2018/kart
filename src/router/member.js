b9051fccad67c8da98e59ca3be52983b42270c09
qingliu <qingliu@linkdoc.com>
2020-05-02 15:36:59 +0800

    增加会员信息

--- src/router/member.js
+++ src/router/member.js
@@ -3,46 +3,48 @@
  */
 export default [
   {
-    path: '/member',
+    path: 'member',
     name: 'member',
-    // component: { render(h) { return h('router-view'); } },
-    component: (resolve) => require(['@/views/member/MemberList.vue'], resolve),
+    component: { render(h) { return h('router-view'); } },
+    // component: (resolve) => require(['@/views/member/MemberList.vue'], resolve),
     meta: {
       title: '会员管理',
       icon: 'icon-setting'
     },
     children: [
-      // {
-      //   path: 'memberList',
-      //   name: 'memberList',
-      //   component: (resolve) => require(['@/views/member/MemberList.vue'], resolve),
-      //   meta: {
-      //     title: '会员列表',
-      //     icon: 'icon-setting'
-      //   }
-      // },
-      // {
-      //   path: 'memberAdd',
-      //   name: 'memberAdd',
-      //   component: (resolve) => require(['@/views/member/MemberForm.vue'], resolve),
-      //   meta: {
-      //     title: '添加会员',
-      //     hideInMenu: true,
-      //     activeMenu: 'memberList',
-      //     icon: 'icon-setting'
-      //   }
-      // },
-      // {
-      //   path: 'memberEdit/:id',
-      //   name: 'memberEdit',
-      //   component: (resolve) => require(['@/views/member/MemberForm.vue'], resolve),
-      //   meta: {
-      //     title: '编辑会员',
-      //     hideInMenu: true,
-      //     activeMenu: 'memberList',
-      //     icon: 'icon-setting'
-      //   }
-      // },
+      {
+        path: 'memberList',
+        name: 'memberList',
+        component: (resolve) => require(['@/views/member/MemberList.vue'], resolve),
+        meta: {
+          title: '会员列表',
+          icon: 'icon-setting',
+          hideInMenu: true,
+          hideInBread: true,
+        }
+      },
+      {
+        path: 'memberAdd',
+        name: 'memberAdd',
+        component: (resolve) => require(['@/views/member/MemberForm.vue'], resolve),
+        meta: {
+          title: '添加会员',
+          hideInMenu: true,
+          activeMenu: 'memberList',
+          icon: 'icon-setting'
+        }
+      },
+      {
+        path: 'memberEdit/:id',
+        name: 'memberEdit',
+        component: (resolve) => require(['@/views/member/MemberForm.vue'], resolve),
+        meta: {
+          title: '编辑会员',
+          hideInMenu: true,
+          activeMenu: 'memberList',
+          icon: 'icon-setting'
+        }
+      },
     ]
   }
 ];
